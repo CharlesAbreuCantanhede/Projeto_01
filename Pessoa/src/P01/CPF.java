cbc068ea1a1bf0b4398ec1caac244c42d75c1da2
CharlesCantanhede <122974602+CharlesAbreuCantanhede@users.noreply.github.com>
2023-01-28 18:38:58 -0300

    Alterando Commit

---
+++ Pessoa/src/P01/CPF.java
@@ -0,0 +1,36 @@
+package P01;
+
+public class CPF {
+   private String nome;
+   
+   private String sobremone;
+   
+   private float  Altura ;
+
+protected String getNome() {
+	return nome;
+}
+
+protected void setNome(String nome) {
+	this.nome = nome;
+}
+
+protected String getSobremone() {
+	return sobremone;
+}
+
+protected void setSobremone(String sobremone) {
+	this.sobremone = sobremone;
+}
+
+protected float getAltura() {
+	return Altura;
+}
+
+protected void setAltura(float altura) {
+	Altura = altura;
+}
+   
+   
+   
+}
