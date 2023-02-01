ba0cc4e9f52740058417bf6cd24b4f6f3e303063
CharlesCantanhede <122974602+CharlesAbreuCantanhede@users.noreply.github.com>
2023-02-01 07:27:14 -0300

    Logicas IF e ElSE !

--- Pessoa/src/P01/PrimeiraClasseJava.java
+++ Pessoa/src/P01/PrimeiraClasseJava.java
@@ -1,29 +1,29 @@
 package P01;
 
 public class PrimeiraClasseJava {
-	
 
 	@SuppressWarnings("unused")
 	public static void main(String[] args) {
+
+		int nota1 = 10;
+		
+		int nota2 = 60;
+		
+		int nota3 = 70;
+		
+		int nota4 = 80;
+		
+		int media = 0;
+		
+		media = (nota1 + nota2 + nota3) / 3;
 		
 		
-		/*Concatenação  = Unir ou juntar dados*/
-		
-		
-		String nome = "Charles A Cantanhede ";
-		
-		String cpf = "498.458.498.12";
-		
-		String telefone = "(63)999471691";
-		
-		String endereço = "Tocantins";
-		
-		String saida = "Meu nome é " + nome + "e o CPF: " +
-		cpf + "meu contato e : " + telefone + " e eu moro em " + 
-		endereço ;
-		
-		System.out.print(saida);
-		
-		
+		if(media >= 70) {
+			System.out.println("Aluno aprovado " + media );
+		} else if (media >= 40 && media <= 69) {
+			System.out.println("Aluno em recuperação " + media );
+		} else {
+			System.out.println("Aluno reprovado " + media);
+		}
 	}
 }
