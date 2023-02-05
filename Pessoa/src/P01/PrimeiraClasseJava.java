e6c73209d9f7bff49e6579b0d212fef44b1ecf1b
CharlesCantanhede <122974602+CharlesAbreuCantanhede@users.noreply.github.com>
2023-02-05 19:34:46 -0300

    Estrutura de Repeticão While do While..

--- Pessoa/src/P01/PrimeiraClasseJava.java
+++ Pessoa/src/P01/PrimeiraClasseJava.java
@@ -5,20 +5,11 @@ package P01;
 public class PrimeiraClasseJava {
 
 	public static void main(String[] args) {
-	 int dia = 0;
-	 switch(dia) {
-	 case 1:
-		System.out.println("Domingo");
-		break;
-	 case 2:
-		 System.out.println("Dias da Semanas ");
-		 break;
-	 case 3:
-		 System.out.println("Final de Semana");
-		 break;
-	 default:
-		 System.out.println("Opção não existe?");
-		 break;
-	 }
+		
+		int numero = 0;
+		while(numero <=10) {
+			System.out.println("O numero é : " + numero);
+			numero++;
+		}
 	}
 }
